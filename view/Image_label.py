778d9a29d6c1f2d4cfa16bb2dd79ff06a1b7467c
Frank Situ <pengu1976@gmail.com>
2020-07-12 23:05:50 +1200

    Improved Image_label class

--- view/Image_label.py
+++ view/Image_label.py
@@ -1,6 +1,7 @@
 ''' Image_label.py
 
 This file will contain a image ready to be displayed in Tkinter.
+Image is stored through a label.
 
 ####### IMPORTANT ###############
 Requires the use of Pillow
@@ -9,35 +10,99 @@ https://pypi.org/project/Pillow/
 
 '''
 
-
-import tkinter as tk
 import logging
 from PIL import Image, ImageTk
 from tkinter import messagebox
 from tkinter import ttk
 
+
 class Image_label(ttk.Label):
-    def __init__(self, parent, file_location, width, height, antialias=Image.ANTIALIAS):
+    def __init__(self, parent, image_name, file_location, width=None, height=None, antialias=Image.ANTIALIAS):
+        ''' Constructor
+
+        :param parent: Parent of where image label will be placed
+        :param image_name: Name of widget (For referencing)
+        :param file_location: File location of image
+        :param width: Custom width (Ignore for default size)
+        :param height: Custom height (Ignore for default size)
+        :param antialias: Custom antialias (Ignore for default Image.Antialias)
+        '''
         super().__init__(parent)
-        rendered_image = ""
+        self.parent = parent
+        self.image_name = image_name
+        self.file_location = file_location
+        self.load_image = ""
+        self.rendered_image = ""
 
-        try: # Load image
-            logging.info("Trying to load image [%s] width, height (%d, %d)", file_location, width, height)
-            load_image = Image.open(file_location)
-            load_image = load_image.resize((width, height), antialias)
-            rendered_image = ImageTk.PhotoImage(load_image)
+        self._load_image()
+        if (width != None) and (height != None):
+            self.resize_image(100, 100)
+        self._update_image()
 
-        except Exception as e: # If unable to load image
-            logging.error("Failed to load image [%s], Replacing with failed to load img", file_location)
-            load_image = Image.open("resources/images/no_image_avaliable.png")
-            load_image = load_image.resize((width, height), antialias)
-            rendered_image = ImageTk.PhotoImage(load_image)
-            self._display_error_message(file_location)
-        self.configure(image=rendered_image)
-        self.image = rendered_image
+    def get_image_name(self):
+        ''' Get name of image label
 
-    def _display_error_message(self, file_location):
-        messagebox.showerror("Error loading image", "Error in loading image \"{}\".\n"
-                                                    " This image has been replaced with an error img."
-                                                    "Note: The program will still continue to work ".format(file_location))
+        :return: String
+        '''
+        return self.image_name
 
+    def resize_image(self, width, height, antialias=Image.ANTIALIAS):
+        ''' Resize the image to a fixed size
+
+        :param width: Custom width
+        :param height: Custom height
+        :param antialias: Custom antialias (Ignore for default Image.Antialias)
+        :return: None
+        '''
+        logging.info("Resizing  [%s} to ({%d},{%d})", self.image_name, width, height)
+        self._load_image()
+        self.load_image = self.load_image.resize((width, height), antialias)
+        self._update_image()
+
+    def resize_image_relative(self, width_percentage, height_percentage):
+        ''' Resize image relative to a parent frame
+
+        :param width_percentage:
+        :param height_percentage:
+        :param parent:
+        :return: None
+        '''
+        self.parent.update()  # Update parent to get accurate data
+        width = self.parent.winfo_width()
+        height = self.parent.winfo_height()
+        new_width = int(width * width_percentage)
+        new_height = int(height * height_percentage)
+        self.resize_image(new_width, new_height)
+
+    def _load_image(self):
+        ''' Private method. Loads the image
+
+        :return: None
+        '''
+        try:  # Load image
+            logging.info("Trying to load image [%s] width, height (%d, %d)", self.file_location)
+            self.load_image = Image.open(self.file_location)
+
+        except Exception as e:  # If unable to load image
+            logging.critical("Failed to load image [%s], Replacing with failed to load img", self.file_location)
+            self.load_image = Image.open("resources/images/no_image_avaliable.png")
+            self._display_error_message()
+
+    def _update_image(self):
+        ''' Update image labe. Must be called after changes to image
+
+        :return: None
+        '''
+        self.rendered_image = ImageTk.PhotoImage(image=self.load_image)
+        self.configure(image=self.rendered_image)
+
+    def _display_error_message(self):
+        ''' Display error message of unable to open image
+
+        :return: None
+        '''
+        messagebox.showerror(
+            "Error loading image", "Error in loading image \"{}\".\n"
+                                   " This image has been replaced with an error img."
+                                   "Note: The program will still continue to work ".format(self.file_location)
+        )
